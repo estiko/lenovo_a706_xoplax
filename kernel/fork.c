055a8fb3e8effb3dd4d3f076d90da4129ee4894a
estiko <estiko@mediaqu.com>
2015-08-26 21:42:53 +0700

    Revert "mm, mempolicy: remove per-process flag"
    This reverts commit aa22f3071a6b0aa4e5eaa1d09bf29b55bb96a380.

--- kernel/fork.c
+++ kernel/fork.c
@@ -1266,6 +1266,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 		p->mempolicy = NULL;
 		goto bad_fork_cleanup_cgroup;
 	}
+	mpol_fix_fork_child_flag(p);
 #endif
 #ifdef CONFIG_CPUSETS
 	p->cpuset_mem_spread_rotor = NUMA_NO_NODE;
