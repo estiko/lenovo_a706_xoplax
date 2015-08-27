841bb8c4e8763125bb833995bfaccc8a54c1be30
estiko <estiko@mediaqu.com>
2015-08-27 23:04:24 +0700

    msm-fb: Fix sysfs node creation in mdp_probe for MDP3 targets
    Signed-off-by: estiko <estiko@mediaqu.com>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -2163,7 +2163,8 @@ static int mdp_on(struct platform_device *pdev)
 
 		vsync_cntrl.dev = mfd->fbi->dev;
 
-		atomic_set(&vsync_cntrl.suspend, 1);
+		atomic_set(&vsync_cntrl.suspend, 0);
+		atomic_set(&vsync_cntrl.vsync_resume, 1);
 	}
 
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
@@ -2796,8 +2797,9 @@ static int mdp_probe(struct platform_device *pdev)
 	pdev_list[pdev_list_cnt++] = pdev;
 	mdp4_extn_disp = 0;
 
-	if (mfd->vsync_init != NULL) {
-		mfd->vsync_init(0);
+	if ((mfd->vsync_init != NULL) || (mdp_rev < MDP_REV_40)) {
+		if (mdp_rev >= MDP_REV_40)
+			mfd->vsync_init(0);
 
 		if (!mfd->vsync_sysfs_created) {
 			mfd->dev_attr.attr.name = "vsync_event";
